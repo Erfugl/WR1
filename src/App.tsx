bd1e037811b2671c1281cd5f459f46fdfe49d42a
Erfugl <erharnes@outlook.com>
2025-10-06 14:13:58 +0200

    2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-//import funfactdataJson from '/funfactdata.json'
-//const funfacts: funfactdata[] = funfactdataJson
+import { useEffect, useState } from 'react'
+
+type FunFact = {
+  fact: string,
+  img: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [funFact, setFunFact] = useState<FunFact | null>(null)
+
+  useEffect(() => {
+    //Hente data fra funfactdata.json hint: fetch
+    fetch("/funfactdata.json")
+      .then(result => result.json())
+      .then(jsonResult => {
+        const funFactsList: FunFact[] = jsonResult.funfacts
+        setFunFact(randomFact(funFactsList))
+
+      })
+    //Sette et interval for å oppdatere et tilfeldig valg element. hint: setInterval
+  }, [])
+
+  function randomFact(factList: FunFact[]) {
+    const index = Math.floor(Math.random() * factList.length)
+    return factList[index]
+  }
 
   return (
     <>
@@ -15,8 +32,9 @@ function App() {
       <h2>Here are som fun facts about me!</h2>
       <button>New fact</button>
       <div id="fact">
-        <p>I am related to a previous dictator of Guatemala: Kjell Laugerud</p>
+        <p>{funFact?.fact || "...loading"}</p>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Foto_oficial_de_Presidente_Kjell_Eugenio_Laugerud_Garcia.jpg/875px-Foto_oficial_de_Presidente_Kjell_Eugenio_Laugerud_Garcia.jpg" alt="Kjell Laugerud" />
+        <p>{funFact?.img || "...loading"} </p>
       </div>
     </>
   )
